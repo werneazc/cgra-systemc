737676634474ad92161a2bfeeeb08fb91d10db5f
andrewerner <andre.werner-w2m@ruhr-uni-bochum.de>
2018-11-01 15:29:07 +0100

    sc_main: This is a sc_main variante to test the data input cache
    Only a small data input cache instance and the corresponding TB is used.
    The trace results are written to dic_test.vcd file.
    
    Signed-off-by: andrewerner <andre.werner-w2m@ruhr-uni-bochum.de>

--- src/sc_main.cpp
+++ src/sc_main.cpp
@@ -8,106 +8,72 @@
 #include <cstdlib>
 #include <systemc>
 #include <array>
-#include "TB_VCGRA.h"
+#include "TB_DataInCache.h"
+#include "DataInCache.h"
 
 int sc_main(int argc, char* arcv[])
 {
 	//include TB
-	auto tb_vcgra = new cgra::TestBench_VCGRA{"VCGRA_TestBench"};
+	auto tb_dic = new cgra::TB_DataInCache{"TestBench_DataInCache"};
 
 //#############################################################################
 
 	//instantiate modules
-	auto vcgra = new cgra::VCGRA{"Small_VCGRA"};
+	auto dic = new cgra::DataInCache<16, 4, 2>{"DataInCache"};
 
 //#############################################################################
 
 	//signals
 	sc_core::sc_clock clk{"clk", 4, sc_core::SC_NS};
-	sc_core::sc_signal<cgra::VCGRA::start_type_t> start{"start"};
-	sc_core::sc_signal<cgra::VCGRA::reset_type_t> rst{"rst"};
-	sc_core::sc_signal<cgra::VCGRA::ready_type_t> ready{"ready"};
-	std::array<sc_core::sc_signal<cgra::VCGRA::input_type_t>, 8> inputs;
-	std::array<sc_core::sc_signal<cgra::VCGRA::output_type_t>, 4> results;
-	sc_core::sc_signal<cgra::VCGRA::pe_config_cache_type_t::write_enable_type_t> write_pe{"write_pe"};
-	sc_core::sc_signal<cgra::VCGRA::pe_config_cache_type_t::select_type_t> slct_in_pe{"slct_in_pe"};
-	sc_core::sc_signal<cgra::VCGRA::pe_config_cache_type_t::select_type_t> slct_out_pe{"slct_out_pe"};
-	sc_core::sc_signal<cgra::VCGRA::ch_config_cache_type_t::write_enable_type_t> write_ch{"write_ch"};
-	sc_core::sc_signal<cgra::VCGRA::ch_config_cache_type_t::select_type_t> slct_in_ch{"slct_in_ch"};
-	sc_core::sc_signal<cgra::VCGRA::ch_config_cache_type_t::select_type_t> slct_out_ch{"slct_out_ch"};
-	sc_core::sc_signal<cgra::VCGRA::pe_config_cache_type_t::stream_type_t> pe_config_stream{"pe_config_stream"};
-	sc_core::sc_signal<cgra::VCGRA::ch_config_cache_type_t::stream_type_t> ch_config_stream{"ch_config_stream"};
-	sc_core::sc_signal<cgra::VCGRA::pe_config_cache_type_t::ack_type_t> ack_pe{"ack_pe"};
-	sc_core::sc_signal<cgra::VCGRA::ch_config_cache_type_t::ack_type_t> ack_ch{"ack_ch"};
+	sc_core::sc_signal<cgra::TB_DataInCache::stream_type_t> s_dataStream{"DataStream"};
+	sc_core::sc_signal<cgra::TB_DataInCache::select_lines_type_t> s_slt_in{"Select_in"};
+	sc_core::sc_signal<cgra::TB_DataInCache::select_lines_type_t> s_slt_out{"Select_out"};
+	sc_core::sc_signal<cgra::TB_DataInCache::select_value_type_t> s_slt_place{"Select_Place"};
+	sc_core::sc_signal<cgra::TB_DataInCache::write_enable_type_t> s_write{"Write"};
+	sc_core::sc_signal<cgra::TB_DataInCache::ack_type_t> s_ack{"Acknowledge"};
+	std::array<sc_core::sc_signal<cgra::TB_DataInCache::value_type_t>, 4> s_currentValues;
 
 //#############################################################################
 
 	//Port bindings
 
 	//clock
-	tb_vcgra->clk.bind(clk);
-	vcgra->clk.bind(clk);
+	tb_dic->sClk.bind(clk);
+	dic->clk.bind(clk);
 
 	//control signals
-	tb_vcgra->start.bind(start);
-	tb_vcgra->ready.bind(ready);
-	tb_vcgra->rst.bind(rst);
-	tb_vcgra->write_pe_cc.bind(write_pe);
-	tb_vcgra->ack_pe_cc.bind(ack_pe);
-	tb_vcgra->slct_in_pe_cc.bind(slct_in_pe);
-	tb_vcgra->slct_out_pe_cc.bind(slct_out_pe);
-	tb_vcgra->write_ch_cc.bind(write_ch);
-	tb_vcgra->slct_in_ch_cc.bind(slct_in_ch);
-	tb_vcgra->slct_out_ch_cc.bind(slct_out_ch);
-	tb_vcgra->ack_ch_cc.bind(ack_ch);
-	vcgra->start.bind(start);
-	vcgra->ready.bind(ready);
-	vcgra->rst.bind(rst);
-	vcgra->write_pe_cc.bind(write_pe);
-	vcgra->ack_pe_cc.bind(ack_pe);
-	vcgra->slct_in_pe_cc.bind(slct_in_pe);
-	vcgra->slct_out_pe_cc.bind(slct_out_pe);
-	vcgra->write_ch_cc.bind(write_ch);
-	vcgra->slct_in_ch_cc.bind(slct_in_ch);
-	vcgra->slct_out_ch_cc.bind(slct_out_ch);
-	vcgra->ack_ch_cc.bind(ack_ch);
+	tb_dic->sSlt_in.bind(s_slt_in);
+	tb_dic->sSlt_out.bind(s_slt_out);
+	tb_dic->sSlt_place.bind(s_slt_place);
+	tb_dic->sWrite.bind(s_write);
+	tb_dic->sAck.bind(s_ack);
+	dic->slt_in.bind(s_slt_in);
+	dic->slt_out.bind(s_slt_out);
+	dic->slt_place.bind(s_slt_place);
+	dic->write.bind(s_write);
+	dic->ack.bind(s_ack);
 
 	//data signals
-	tb_vcgra->pe_config_stream.bind(pe_config_stream);
-	tb_vcgra->ch_config_stream.bind(ch_config_stream);
-	vcgra->pe_config_stream.bind(pe_config_stream);
-	vcgra->ch_config_stream.bind(ch_config_stream);
-	for(uint32_t i = 0; 8 > i; ++i)
-	{
-		tb_vcgra->inputs.at(i).bind(inputs.at(i));
-		vcgra->inputs.at(i).bind(inputs.at(i));
-	}
+	tb_dic->sDataInStream.bind(s_dataStream);
+	dic->dataInStream.bind(s_dataStream);
 	for(uint32_t i = 0; 4 > i; ++i)
 	{
-		tb_vcgra->results.at(i).bind(results.at(i));
-		vcgra->results.at(i).bind(results.at(i));
+		tb_dic->sCurrentValues.at(i).bind(s_currentValues.at(i));
+		dic->currentValues.at(i).bind(s_currentValues.at(i));
 	}
 
 //#############################################################################
 
 	//create and setup trace file;
-	auto fp_vcgra = sc_core::sc_create_vcd_trace_file("vcgra_test");
+	auto fp_vcgra = sc_core::sc_create_vcd_trace_file("dic_test");
 
 	sc_core::sc_trace(fp_vcgra,clk,"clock");
-	sc_core::sc_trace(fp_vcgra,start,"start");
-	sc_core::sc_trace(fp_vcgra,rst,"rst");
-	sc_core::sc_trace(fp_vcgra,ready,"ready");
-	sc_core::sc_trace(fp_vcgra,write_pe,"write_pe");
-	sc_core::sc_trace(fp_vcgra,ack_pe,"ack_pe,");
-	sc_core::sc_trace(fp_vcgra,slct_in_pe,"slct_in_pe");
-	sc_core::sc_trace(fp_vcgra,slct_out_pe,"slct_out_pe");
-	sc_core::sc_trace(fp_vcgra,write_ch,"write_ch");
-	sc_core::sc_trace(fp_vcgra,ack_ch,"ack_ch,");
-	sc_core::sc_trace(fp_vcgra,slct_in_ch,"slct_in_ch");
-	sc_core::sc_trace(fp_vcgra,slct_out_ch,"slct_out_ch");
-	sc_core::sc_trace(fp_vcgra,pe_config_stream,"pe_config_stream");
-	sc_core::sc_trace(fp_vcgra,ch_config_stream,"ch_config_stream");
-
+	sc_core::sc_trace(fp_vcgra,s_slt_in,"Select_In");
+	sc_core::sc_trace(fp_vcgra,s_slt_out,"Select_Out");
+	sc_core::sc_trace(fp_vcgra,s_slt_place,"Select_Place");
+	sc_core::sc_trace(fp_vcgra,s_write,"Write");
+	sc_core::sc_trace(fp_vcgra,s_ack,"Acknowledge");
+	sc_core::sc_trace(fp_vcgra,s_dataStream,"DataStream");
 
 	{	//Lambda function to create port name in trace file
 		uint8_t i = 0;
@@ -118,12 +84,9 @@ int sc_main(int argc, char* arcv[])
 		};
 
 		//Trace inputs
-		for(auto& in : inputs)
-			sc_core::sc_trace(fp_vcgra,in, concat(i++, "input_"));
+		for(auto& in : s_currentValues)
+			sc_core::sc_trace(fp_vcgra,in, concat(i++, "place_"));
 
-		i = 0;
-		for(auto& res : results)
-			sc_core::sc_trace(fp_vcgra, res, concat(i++, "result_"));
 	}
 //#############################################################################
 
@@ -131,7 +94,8 @@ int sc_main(int argc, char* arcv[])
 	sc_core::sc_start(1,sc_core::SC_MS);
 
 #ifdef DEBUG
-	vcgra->dump();
+	tb_dic->dump();
+	dic->dump();
 #endif //DEBUG
 
 //#############################################################################
