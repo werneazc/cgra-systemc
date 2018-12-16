eb9454a69d21ed838e6ced8cbd56451995a14e23
andrewerner <andre.werner-w2m@ruhr-uni-bochum.de>
2018-12-16 12:57:14 +0100

    Management_Unit: Update state machine
    Signed-off-by: andrewerner <andre.werner-w2m@ruhr-uni-bochum.de>

--- header/Management_Unit.h
+++ header/Management_Unit.h
@@ -5,8 +5,12 @@
 #include <cstdint>
 #include <iostream>
 #include <array>
+#include <utility>
 #include "Typedef.h"
 #include "CommandInterpreter.h"
+#include "DataInCache.h"
+#include "DataOutCache.h"
+#include "ConfigurationCache.h"
 #include "MMU.h"
 
 namespace cgra {
@@ -31,6 +35,8 @@ public:
 	//!< \brief ManagementUnit clock type
 	typedef bool run_type_t;
 	//!< \brief ManagementUnit run type
+	typedef bool finish_type_t;
+	//!< \brief ManagementUnit finish type
 	typedef bool pause_type_t;
 	//!< \brief ManagementUnit pause type
 	typedef cgra::reset_type_t reset_type_t;
@@ -41,11 +47,21 @@ public:
 	//!< \brief Start type for VCGRA
 	typedef CommandInterpreter::assembler_type_t assembler_type_t;
 	//!< \brief Type definition for assembler command
+	typedef std::pair<sc_core::sc_out<cgra::data_input_cache_type_t::select_lines_type_t>, sc_core::sc_out<cgra::data_input_cache_type_t::select_lines_type_t>> dic_select_type_t;
+	//!< \brief Select type for input and output line of DataInCache
+	typedef std::pair<sc_core::sc_out<cgra::data_output_cache_type_t::select_lines_type_t>, sc_core::sc_out<cgra::data_output_cache_type_t::select_lines_type_t>> doc_select_type_t;
+	//!< \brief Select type for input and output line of DataOutCache
+	typedef std::pair<sc_core::sc_out<cgra::pe_config_cache_type_t::select_type_t>, sc_core::sc_out<cgra::pe_config_cache_type_t::select_type_t>> pe_cc_select_type_t;
+	//!< \brief Select type for input and output line of Processing_Element ConfigurationCache
+	typedef std::pair<sc_core::sc_out<cgra::ch_config_cache_type_t::select_type_t>, sc_core::sc_out<cgra::ch_config_cache_type_t::select_type_t>> ch_cc_select_type_t;
+	//!< \brief Select type for input and output line of VirtualChannel ConfigurationCache
 
 	//Entity Ports
 	//============
 	sc_core::sc_in<run_type_t> run{"run"};
 	//!< \brief ManagementUnit run port to start execution
+	sc_core::sc_out<finish_type_t> finish{"finish"};
+	//!< \brief ManagementUnit finish port to indicate finish state
 	sc_core::sc_in<pause_type_t> pause{"pause"};
 	//!< \brief ManagementUnit pause port to pause execution
 	sc_core::sc_in<reset_type_t> rst{"reset"};
@@ -58,8 +74,6 @@ public:
 	//!< \brief (Synchronization) Start signal port for VCGRA
 	sc_core::sc_out<CommandInterpreter::address_type_t> address{"Address"};
 	//!< \brief Shared memory (start) address of data (sequence)
-	sc_core::sc_out<CommandInterpreter::line_type_t> line{"Line"};
-	//!< \brief Cache line number of source/target cache
 	sc_core::sc_out<CommandInterpreter::place_type_t> place{"Place"};
 	//!< \brief Place in cache line to store data from or load data to
 	sc_core::sc_out<MMU::cache_select_type_t> cache_select{"Cache_Select"};
@@ -68,7 +82,14 @@ public:
 	//!< \brief If false, shared memory ready to process new command, else shared memory is processing.
 	sc_core::sc_out<MMU::start_type_t> mmu_start{"MMU_start"};
 	//!< \brief If false, shared memory ready to process new command, else shared memory is processing.
-
+	dic_select_type_t dic_select_lines;
+	//!< \brief Select line outputs (select_in, select_out) for DataInCache
+	doc_select_type_t doc_select_lines;
+	//!< \brief Select line outputs (select_in, select_out) for DataOutCache
+	pe_cc_select_type_t pe_cc_select_lines;
+	//!< \brief Select line outputs (select_in, select_out) for Processing_Element ConfigurationCache
+	ch_cc_select_type_t ch_cc_select_lines;
+	//!< \brief Select line outputs (select_in, select_out) for Processing_Element ConfigurationCache
 
 	//Methods
 	//=======
@@ -81,19 +102,66 @@ public:
 	 * General Constructor of ManagementUnit
 	 *
 	 * \param [in] nameA SystemC module name
+	 * \param [in] program_assemblyA Pointer to an array of assembler commands
+	 * \param [in] sizeA Number of assembler commands (array size)
 	 */
-	ManagementUnit(const sc_core::sc_module_name& nameA);
+	ManagementUnit(const sc_core::sc_module_name& nameA, CommandInterpreter::assembler_type_t* const program_assemblyA, const uint64_t sizeA);
 
 	/*!
 	 * \brief Destructor
 	 */
 	virtual ~ManagementUnit() = default;
 
+	/*!
+	 * \brief Initialize output signals of module
+	 */
+	virtual void end_of_elaboration() override;
 
 	/*!
-	 * \brief Internal state machine of ManagingUnit
+	 * \brief Print kind of SystemC module
+	 */
+	virtual const char* kind() const override
+	{ return "Management Unit";	}
+
+	/*!
+	 * \brief Print name of ManagementUnit
+	 *
+	 * \param[out] os Define used outstream [default: std::cout]
+	 */
+	virtual void print(std::ostream& os = std::cout) const override
+	{ os << this->name(); return; }
+
+	/*!
+	 * Dump ManagementUnit information
+	 *
+	 * \param[out] os Define used outstream [default: std::cout]
+	 */
+	virtual void dump(std::ostream& os = std::cout) const override;
+
+	/*!
+	 * \brief Internal state machine of ManagementUnit
 	 */
 	void state_machine();
+
+	/* Public Members:
+	 * ==============
+	 *
+	 * These members are set to public to enable tracing of the signals.
+	 */
+
+	//Internal signals
+	sc_core::sc_buffer<CommandInterpreter::command_type_t> m_currentCommand{"CurrentCommand"};
+	//!< \brief Local buffer for latest command
+	sc_core::sc_buffer<CommandInterpreter::assembler_type_t> m_currentAssembler{"CurrentAssembler"};
+	//!< \brief Local buffer for latest assembler command
+	sc_core::sc_buffer<CommandInterpreter::address_type_t> m_currentAddress{"CurrentAddress"};
+	//!< \brief Local buffer for latest address in shared memory
+	sc_core::sc_buffer<CommandInterpreter::line_type_t> m_currentLine{"CurrentLine"};
+	//!< \brief Local buffer for latest line number in data cache
+	sc_core::sc_buffer<CommandInterpreter::place_type_t> m_currentPlace{"CurrentPlace"};
+	//!< \brief Local buffer for latest place number in data cache
+	sc_core::sc_signal<bool> m_readyInterrupt{"VCGRA_Ready_Interrupt"};
+	//!< \brief Interrupt buffer for VCGRA ready signal if ManagementUnit does not wait active.
 
 private:
 
@@ -106,21 +174,8 @@ private:
 	assembler_type_t* m_programPointer;
 	//!< \brief Program pointer for assembler program
 
+
 	//Internal signals
-	sc_core::sc_buffer<CommandInterpreter::assembler_type_t> m_currentAssembler{"CurrentAssembler"};
-	//!< \brief Local buffer for latest assembler command
-	sc_core::sc_buffer<CommandInterpreter::command_type_t> m_currentCommand{"CurrentCommand"};
-	//!< \brief Local buffer for latest command
-	sc_core::sc_buffer<CommandInterpreter::address_type_t> m_currentAddress{"CurrentAddress"};
-	//!< \brief Local buffer for latest address in shared memory
-	sc_core::sc_buffer<CommandInterpreter::line_type_t> m_currentLine{"CurrentLine"};
-	//!< \brief Local buffer for latest line number in data cache
-	sc_core::sc_buffer<CommandInterpreter::place_type_t> m_currentPlace{"CurrentPlace"};
-	//!< \brief Local buffer for latest place number in data cache
-	sc_core::sc_signal<bool> m_readyInterrupt{"VCGRA_Ready_Interrupt"};
-	//!< \brief Interrupt buffer for VCGRA ready signal if ManagementUnit does not wait active.
-
-
 	/*! ManagementUnit processing status */
 	enum ACTIVE_STATE : uint8_t
 	{
@@ -137,7 +192,9 @@ private:
 	enum STATE : uint8_t
 	{
 		NOOP,			//!< \brief No operation
-		START,			//!< \brief Start VCGRA
+		ADAPT_PP,		//!< \brief Adapt program pointer (PP)
+		FETCH,			//!< \brief Load next command from assembler command memory
+		DECODE,			//!< \brief Decode assembler command and set next state
 		WAIT_READY,		//!< \brief Pause until VCGRA ready signal
 		LOADD,			//!< \brief Load data to data cache
 		LOADDA,			//!< \brief Load a whole cache line to data cache
@@ -145,10 +202,14 @@ private:
 		STOREDA,		//!< \brief Store a whole cache line to shared memory
 		LOADPC,			//!< \brief Load complete configuration cache of PEs
 		LOADCC,			//!< \brief Load complete configuration cache of vCh
-		FETCH,			//!< \brief Load next command from assembler command memory
-		DECODE,			//!< \brief Decode assembler command and set next state
-		ADAPT_PP,		//!< \brief Adapt program pointer (PP)
-		WAIT_MMU		//!< \brief Synchronization state with MMU
+		START,			//!< \brief Start VCGRA
+		FINISH,			//!< \brief Finish VCGRA execution
+		WAIT_MMU,		//!< \brief Synchronization state with MMU
+		CONT_MMU,		//!< \brief Continue execution after wait for MMU
+		SLCT_DIC_LINE,	//!< \brief Select current cache line for DataInCache
+		SLCT_DOC_LINE,	//!< \brief Select current cache line for DataOutCache
+		SLCT_PECC_LINE,	//!< \brief Select current cache line for Processing_Element ConfigurationCache
+		SLCT_CHCC_LINE	//!< \brief Select current cache line for VirtualChannel ConfigurationCache
 	} m_current_state;
 	//!< Current state of ManagementUnit state machine
 
@@ -244,6 +305,18 @@ private:
 	 *
 	 */
 	void start_mmu();
+
+	/*!
+	 * \brief Show finished execution of Assembler program
+	 *
+	 * \details:
+	 * This function indicates a finished execution of an
+	 * Assembler command program. It raises the finish signal
+	 * for at least one clock cycle or until the run signal
+	 * has a negative edge.
+	 */
+	void show_finish_state();
+
 };
 
 } // namespace cgra
--- src/Management_Unit.cpp
+++ src/Management_Unit.cpp
@@ -19,7 +19,9 @@ static constexpr const char* integer2binaryCstr(const uint32_t numberA)
 
 
 
-ManagementUnit::ManagementUnit(const sc_core::sc_module_name& nameA)
+ManagementUnit::ManagementUnit(const sc_core::sc_module_name& nameA,
+		CommandInterpreter::assembler_type_t* const program_assemblyA,
+		const uint64_t sizeA)
 	: sc_core::sc_module(nameA), m_programPointer(&m_programMemory.front()),
 	   m_activeState(ACTIVE_STATE::STOP), m_current_state(STATE::NOOP)
 {
@@ -28,14 +30,19 @@ ManagementUnit::ManagementUnit(const sc_core::sc_module_name& nameA)
 	sensitive << clk.pos();
 	SC_METHOD(wait_ready);
 	sensitive << ready.pos();
+	dont_initialize();
 	SC_METHOD(pause_interrupt);
 	sensitive << pause.value_changed();
+	dont_initialize();
 	SC_METHOD(reset_interrupt);
 	sensitive << rst.neg();
+	dont_initialize();
 	SC_METHOD(run_interrupt);
 	sensitive << run.pos();
+	dont_initialize();
 	SC_METHOD(wait_mmu_ready);
 	sensitive << mmu_ready.neg();
+	dont_initialize();
 
 	//Wire signal connections
 	m_cInterpreter.assembler.bind(m_currentAssembler);
@@ -43,6 +50,25 @@ ManagementUnit::ManagementUnit(const sc_core::sc_module_name& nameA)
 	m_cInterpreter.address.bind(m_currentAddress);
 	m_cInterpreter.line.bind(m_currentLine);
 	m_cInterpreter.place.bind(m_currentPlace);
+
+	//Store Assembler commands form source in program memory
+	uint64_t t_size;
+	//auto t_ptr = const_cast<CommandInterpreter::assembler_type_t*>(program_assemblyA);
+
+	//Limit program size to program memory.
+	if(sizeA > cgra::cProgramMemorySize)
+	{
+		t_size = cgra::cProgramMemorySize;
+		SC_REPORT_WARNING("ManagementUnit", "Program size exceeds program memory.");
+	}
+	else
+		t_size = sizeA;
+
+	for(uint64_t i = 0; t_size > i; ++i)
+		m_programMemory.at(i) = *(program_assemblyA + i);
+	//memcpy(m_programMemory.data(), t_ptr, t_size * sizeof(CommandInterpreter::assembler_type_t));
+
+	return;
 }
 
 void ManagementUnit::state_machine()
@@ -52,6 +78,7 @@ void ManagementUnit::state_machine()
 		switch (m_current_state)
 		{
 			case STATE::NOOP:
+				m_current_state = STATE::ADAPT_PP;
 				break;
 			case STATE::FETCH:
 				fetch();
@@ -66,11 +93,12 @@ void ManagementUnit::state_machine()
 				start_state();
 				break;
 			case STATE::WAIT_READY:
+			{
 				/*
 				 * If assembler commands are performed in parallel to
 				 * a processing VCGRA, an interrupt is stored. If the
 				 * assembler program reaches a WAIT READY command for the
-				 * next time, it is checked if an interrupt already occurs.
+				 * next time, it is checked if an interrupt already occurred.
 				 * Then, the ManagementUnit will not go into active state WAIT,
 				 * it will start with the next assembler program step.
 				 *
@@ -81,34 +109,31 @@ void ManagementUnit::state_machine()
 				{
 					m_readyInterrupt.write(false);
 					m_current_state = STATE::ADAPT_PP;
-					break;
 				}
 				else
-				{
-				m_activeState = ACTIVE_STATE::WAIT;
+					m_activeState = ACTIVE_STATE::WAIT;
 				break;
-				}
+			}
 			case STATE::WAIT_MMU:
 				m_activeState = ACTIVE_STATE::WAIT;
 				break;
+			case STATE::CONT_MMU:
+				start_mmu();
+				m_current_state = STATE::ADAPT_PP;
+				break;
 			case STATE::LOADD:
-				start_mmu();
-				break;
 			case STATE::LOADDA:
-				start_mmu();
-				break;
 			case STATE::LOADPC:
-				start_mmu();
-				break;
 			case STATE::LOADCC:
-				start_mmu();
-				break;
 			case STATE::STORED:
-				start_mmu();
-				break;
 			case STATE::STOREDA:
 				start_mmu();
 				break;
+			case STATE::FINISH:
+			{
+				show_finish_state();
+				break;
+			}
 			default:
 				m_current_state = STATE::NOOP;
 				SC_REPORT_WARNING("MU State","Unknown State. No operation performed.");
@@ -117,9 +142,11 @@ void ManagementUnit::state_machine()
 	}
 	else if(ACTIVE_STATE::ERROR == m_activeState)
 	{
-		//#TODO: Create State machine diagram and error dump
+		this->dump();
 		SC_REPORT_FATAL("MU", "Runtime error");
 	}
+
+	return;
 }
 
 void ManagementUnit::fetch()
@@ -127,6 +154,8 @@ void ManagementUnit::fetch()
 	//Load current assembler command to decode module
 	m_currentAssembler.write(*m_programPointer);
 	m_current_state = STATE::DECODE;
+
+	return;
 }
 
 void ManagementUnit::decode()
@@ -139,27 +168,49 @@ void ManagementUnit::decode()
 	{
 	case STATE::LOADD:
 		cache_select.write(integer2binaryCstr<3>(MMU::CACHE_TYPE::DATA_INPUT));
+		dic_select_lines.first.write(m_cInterpreter.line.read().to_uint());
 		place.write(m_currentPlace);
 		break;
 	case STATE::LOADDA:
 		cache_select.write(integer2binaryCstr<3>(MMU::CACHE_TYPE::DATA_INPUT));
+		dic_select_lines.first.write(m_cInterpreter.line.read().to_uint());
 		place.write(integer2binaryCstr<7>(127));
+		break;
+	case STATE::SLCT_DIC_LINE:
+		dic_select_lines.second.write(m_cInterpreter.line.read().to_uint());
+		m_current_state = STATE::ADAPT_PP;
 		break;
 	case STATE::STORED:
 		cache_select.write(integer2binaryCstr<3>(MMU::CACHE_TYPE::DATA_OUTPUT));
+		doc_select_lines.second.write(m_cInterpreter.line.read().to_uint());
 		place.write(m_currentPlace);
 		break;
 	case STATE::STOREDA:
 		cache_select.write(integer2binaryCstr<3>(MMU::CACHE_TYPE::DATA_OUTPUT));
+		doc_select_lines.second.write(m_cInterpreter.line.read().to_uint());
 		place.write(integer2binaryCstr<7>(127));
+		break;
+	case STATE::SLCT_DOC_LINE:
+		doc_select_lines.first.write(m_cInterpreter.line.read().to_uint());
+		m_current_state = STATE::ADAPT_PP;
 		break;
 	case STATE::LOADPC:
 		cache_select.write(integer2binaryCstr<3>(MMU::CACHE_TYPE::CONF_PE));
+		pe_cc_select_lines.first.write(m_cInterpreter.line.read().to_uint());
 		place.write(m_currentPlace);
+		break;
+	case STATE::SLCT_PECC_LINE:
+		pe_cc_select_lines.second.write(m_cInterpreter.line.read().to_uint());
+		m_current_state = STATE::ADAPT_PP;
 		break;
 	case STATE::LOADCC:
 		cache_select.write(integer2binaryCstr<3>(MMU::CACHE_TYPE::CONF_CC));
+		ch_cc_select_lines.first.write(m_cInterpreter.line.read().to_uint());
 		place.write(m_currentPlace);
+		break;
+	case STATE::SLCT_CHCC_LINE:
+		ch_cc_select_lines.second.write(m_cInterpreter.line.read().to_uint());
+		m_current_state = STATE::ADAPT_PP;
 		break;
 	default:
 		cache_select.write(integer2binaryCstr<3>(MMU::CACHE_TYPE::NONE));
@@ -167,13 +218,16 @@ void ManagementUnit::decode()
 		break;
 	}
 	address.write(m_currentAddress);
-	line.write(m_currentLine);
+
+	return;
 }
 
 void ManagementUnit::adapt_prog_pointer()
 {
 	++m_programPointer;
 	m_current_state = STATE::FETCH;
+
+	return;
 }
 
 void ManagementUnit::start_state()
@@ -190,9 +244,13 @@ void ManagementUnit::start_state()
 		start.write(false);
 		m_current_state = STATE::ADAPT_PP;
 	}
-	//TODO: There is no signal which shows that the VCGRA is currently in process. The assembler needs to ensure
+	/*There is no signal which shows that the VCGRA is currently in process.
+	* The assembler needs to ensure that the VCGRA is started again only if the ready signal already occurred.
+	*/
 	else
 		start.write(true);
+
+	return;
 }
 
 void ManagementUnit::wait_ready()
@@ -210,28 +268,195 @@ void ManagementUnit::wait_ready()
 		if(!m_readyInterrupt.read())
 		m_readyInterrupt.write(true);
 	}
+
+	return;
+}
+
+void ManagementUnit::end_of_elaboration()
+{
+	start.write(false);
+	mmu_start.write(false);
+	address.write(0);
+	place.write(0);
+	finish.write(false);
+	cache_select.write(MMU::CACHE_TYPE::NONE);
+	m_currentCommand.write(0);
+
+	dic_select_lines.first.write(0);
+	dic_select_lines.second.write(1);
+	doc_select_lines.first.write(0);
+	doc_select_lines.second.write(1);
+	pe_cc_select_lines.first.write(0);
+	pe_cc_select_lines.second.write(1);
+	ch_cc_select_lines.first.write(0);
+	ch_cc_select_lines.second.write(1);
+}
+
+void ManagementUnit::dump(std::ostream& os) const
+{
+	os << name() << ": " << kind() << std::endl;
+	os << "Current start status:\t\t" << std::setw(3) << std::boolalpha << start.read() << std::endl;
+	os << "Current ready status:\t\t" << std::setw(3) << std::boolalpha << ready.read() << std::endl;
+	os << "Current run status:\t\t" << std::setw(3) << std::boolalpha << run.read() << std::endl;
+	os << "Current pause status:\t\t" << std::setw(3) << std::boolalpha << pause.read() << std::endl;
+	os << "Current reset status:\t\t" << std::setw(3) << std::boolalpha << rst.read() << std::endl;
+	os << "Current MMU start status:\t" << std::setw(3) << std::boolalpha << mmu_start.read() << std::endl;
+	os << "Current MMU ready status:\t" << std::setw(3) << std::boolalpha << mmu_ready.read() << std::endl;
+	os << "Current RUN STATE:\t\t";
+	switch(static_cast<enum STATE>(m_currentCommand.read().to_uint()))
+	{
+		case STATE::NOOP:
+			os << "NOOP" << std::endl;
+			break;
+		case STATE::FETCH:
+			os << "FETCH" << std::endl;
+			break;
+		case STATE::DECODE:
+			os << "DECODE" << std::endl;
+			break;
+		case STATE::ADAPT_PP:
+			os << "ADAPT_PROGRAM_POINTER" << std::endl;
+			break;
+		case STATE::LOADD:
+			os << "Load Data Value" << std::endl;
+			break;
+		case STATE::LOADDA:
+			os << "Load data value for a cache line" << std::endl;
+			break;
+		case STATE::LOADCC:
+			os << "Load virtual channel configuration cache" << std::endl;
+			break;
+		case STATE::LOADPC:
+			os << "Load processing element configuration cache" << std::endl;
+			break;
+		case STATE::START:
+			os << "Start VCGRA processing" << std::endl;
+			break;
+		case STATE::STORED:
+			os << "Read data value" << std::endl;
+			break;
+		case STATE::STOREDA:
+			os << "Read data values of cache line" << std::endl;
+			break;
+		case STATE::WAIT_MMU:
+			os << "Wait for MMU to be ready" << std::endl;
+			break;
+		case STATE::CONT_MMU:
+			os << "Continue execution after wait for MMU." << std::endl;
+			break;
+		case STATE::WAIT_READY:
+			os << "Wait for VCGRA to be ready" << std::endl;
+			break;
+		case STATE::SLCT_DIC_LINE:
+			os << "Select active data input cache outline." << std::endl;
+			break;
+		case STATE::SLCT_DOC_LINE:
+			os << "Select active data output cache inline." << std::endl;
+			break;
+		case STATE::SLCT_PECC_LINE:
+			os << "Select active PE configuration cache outline." << std::endl;
+			break;
+		case STATE::SLCT_CHCC_LINE:
+			os << "Select active virtual channel configuration cache outline." << std::endl;
+			break;
+		default:
+			os << "Unknown cache type." << std::endl;
+			break;
+	}
+	os << "Current Address:\t\t" << std::setw(3) << address.read().to_string(sc_dt::SC_DEC, false) << std::endl;
+	os << "Current place:\t\t\t" << std::setw(3) << place.read().to_string(sc_dt::SC_DEC, false) << std::endl;
+	os << "Current Cache:\t\t\t";
+	switch(static_cast<MMU::CACHE_TYPE>(cache_select.read().to_uint()))
+	{
+	case MMU::CACHE_TYPE::DATA_INPUT:
+		os << "DATA_INPUT";
+		break;
+	case MMU::CACHE_TYPE::DATA_OUTPUT:
+		os << "DATA_OUTPUT";
+		break;
+	case MMU::CACHE_TYPE::CONF_PE:
+		os << "CONF_PE";
+		break;
+	case MMU::CACHE_TYPE::CONF_CC:
+		os << "DATA_CC";
+		break;
+	case MMU::CACHE_TYPE::NONE:
+		os << "NONE";
+		break;
+	}
+	os << std::endl;
+
+	os << "Current line selections for caches:\n";
+	os << "===================================" << std::endl;
+	os << "Data Input Cache: Select in: " << dic_select_lines.first.read().to_string(sc_dt::SC_DEC);
+	os << "; Select out: " << dic_select_lines.second.read().to_string(sc_dt::SC_DEC) << std::endl;
+	os << "Data Output Cache: Select in: " << doc_select_lines.first.read().to_string(sc_dt::SC_DEC);
+	os << "; Select out: " << doc_select_lines.second.read().to_string(sc_dt::SC_DEC) << std::endl;
+	os << "PE Configuration Cache: Select in: " << pe_cc_select_lines.first.read().to_string(sc_dt::SC_DEC);
+	os << "; Select out: " << pe_cc_select_lines.second.read().to_string(sc_dt::SC_DEC) << std::endl;
+	os << "vCh Configuration Cache: Select in: " << ch_cc_select_lines.first.read().to_string(sc_dt::SC_DEC);
+	os << "; Select out: " << ch_cc_select_lines.second.read().to_string(sc_dt::SC_DEC) << std::endl;
+
+
+	os << "Current ACTIVE STATE:\t\t";
+	switch(m_activeState)
+	{
+	case ACTIVE_STATE::RUN:
+		os << "RUN" << std::endl;
+		break;
+	case ACTIVE_STATE::PAUSE:
+		os << "PAUSE" << std::endl;
+		break;
+	case ACTIVE_STATE::STOP:
+		os << "STOP" << std::endl;
+		break;
+	case ACTIVE_STATE::WAIT:
+		os << "WAIT" << std::endl;
+		break;
+	case ACTIVE_STATE::ERROR:
+		os << "ERROR" << std::endl;
+		break;
+	default:
+		os << "Unknown ACTIVE STATE" << std::endl;
+	}
+
+	return;
+
 }
 
 void ManagementUnit::start_mmu()
 {
 	//Set start signal for MMU High for one clock periode.
 	if(!mmu_start.read())
-		mmu_start.write(true);
-	else
 	{
-		mmu_start.write(false);
-		m_current_state = WAIT_MMU;
+		mmu_start.write(true);
+		m_current_state = STATE::WAIT_MMU;
 	}
+	else
+		mmu_start.write(false);
+
+	return;
 }
 
 void ManagementUnit::pause_interrupt()
 {
-	//Positive edge: Pause processing
-	//Negative edge: Resume processing
-	if(pause.read())
-		m_activeState = ACTIVE_STATE::PAUSE;
+
+	if(ACTIVE_STATE::RUN == m_activeState)
+	{
+		//Positive edge: Pause processing
+		if(pause.read())
+			m_activeState = ACTIVE_STATE::PAUSE;
+	}
+	else if(ACTIVE_STATE::PAUSE == m_activeState)
+	{
+		//Negative edge: Resume processing
+		if(!pause.read())
+			m_activeState = ACTIVE_STATE::RUN;
+	}
 	else
-		m_activeState = ACTIVE_STATE::RUN;
+		SC_REPORT_WARNING("MU", "PAUSE function only available in previous state RUN");
+
+	return;
 }
 
 void ManagementUnit::run_interrupt()
@@ -239,6 +464,8 @@ void ManagementUnit::run_interrupt()
 	//Enable processing of state machine
 	m_activeState = ACTIVE_STATE::RUN;
 	m_current_state = STATE::FETCH;
+
+	return;
 }
 
 void ManagementUnit::reset_interrupt()
@@ -249,21 +476,36 @@ void ManagementUnit::reset_interrupt()
 	//Reset internal registers and program pointer
 	m_current_state = STATE::NOOP;
 	m_programPointer = &m_programMemory.front();
-	m_currentAssembler.write(0);
 
+	return;
 }
 
 void ManagementUnit::wait_mmu_ready()
 {
 	//WAIT for MMU to finish processing
-	if(ACTIVE_STATE::WAIT == m_activeState)
+	if(ACTIVE_STATE::WAIT == m_activeState && mmu_start.read())
 	{
-		m_current_state = STATE::ADAPT_PP;
+		m_current_state = STATE::CONT_MMU;
 		m_activeState = ACTIVE_STATE::RUN;
 	}
 	else
 		m_activeState = ACTIVE_STATE::ERROR;
+
+	return;
 }
 
+void ManagementUnit::show_finish_state()
+{
+	if(finish.read())
+	{
+		m_activeState = ACTIVE_STATE::STOP;
+		m_current_state = STATE::NOOP;
+		finish.write(false);
+	}
+	else
+		finish.write(true);
+
+	return;
+}
 
 } // end namespace cgra
