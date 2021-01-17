177e1e6c13e3ee3bfd4479fa4612f8bcffa359f4
andrewerner <andre.werner-w2m@ruhr-uni-bochum.de>
2021-01-17 15:31:48 +0100

    VCGRA_ONLY: Add project for simulating only the VCGRA without any other comp.
    Signed-off-by: andrewerner <andre.werner-w2m@ruhr-uni-bochum.de>

---
+++ header/testbench_vcgra_only.hpp
@@ -0,0 +1,150 @@
+/*
+ * testbench_vcgra_only.hpp
+ *
+ *  Created on: Dec 04, 2020
+ *      Author: andrewerner
+ */
+
+#include "VCGRA.h"
+#include <systemc>
+#include <vector>
+
+namespace cgra
+{
+
+class Testbench : public sc_core::sc_module
+{
+  public:
+    // Type definitions
+    typedef typename VCGRA::data_input_type_t data_input_type_t;
+    //!< \brief VCGRA data input type
+    typedef typename VCGRA::data_output_type_t data_output_type_t;
+    //!< \brief VCGRA data output type
+
+    // Entity ports
+    sc_core::sc_in<cgra::clock_type_t> clk{"clk"};
+    //!< \brief VCGRA clock port
+    sc_core::sc_out<cgra::start_type_t> start{"start"};
+    //!< \brief VCGRA start port
+    sc_core::sc_out<cgra::reset_type_t> rst{"rst"};
+    //!< \brief VCGRA reset port
+    sc_core::sc_out<cgra::pe_config_type_t> pe_config{"pe_config"};
+    //!< \brief VCGRA PE configuration port
+    sc_core::sc_out<cgra::ch_config_type_t> ch_config{"ch_config"};
+    //!< \brief VCGRA VirtualChannel configuration port
+    std::array<sc_core::sc_out<data_input_type_t>, cgra::cInputChannel_NumOfInputs> data_inputs;
+    //!< \brief VCGRA data inputs to first VirtualChannel level
+    sc_core::sc_in<cgra::ready_type_t> ready{"ready"};
+    //!< \brief VCGRA ready port
+    std::array<sc_core::sc_in<data_output_type_t>, cgra::cPeLevels.back()> data_outputs;
+    //!< \brief VCGRA data outputs
+
+    // Member functions
+    //-----------------
+
+    SC_HAS_PROCESS(Testbench);
+    /**
+     * @brief General construtor
+     *
+     * @param nameA         SystemC module name
+     * @param imagePathA    Path to image in PGM-format
+     */
+    Testbench(const sc_core::sc_module_name &nameA, std::string imagePathA);
+
+    // Deleted constructors
+    Testbench() = delete;
+    //!< @brief SystemC requires unique module name
+    Testbench(const Testbench &rhs) = delete;
+    //!< @brief sc_module cannot being copied
+    Testbench(Testbench &&rhs) = delete;
+    //!< @brief sc_module cannot being moved
+    Testbench &operator=(const Testbench &rhs) = delete;
+    //!< @brief sc_module cannot being copied
+    Testbench &operator=(Testbench &&rhs) = delete;
+    //!< @brief sc_module cannot being moved
+
+    /**
+     * @brief Destroy the Testbench object
+     */
+    ~Testbench() override = default;
+
+    /**
+     * @brief Append new PE configuration
+     *
+     * @param peConfA PE configuration as a byte stream
+     *
+     * @return Index of added configuration
+     */
+    std::size_t appendPeConfiguration(std::vector<uint8_t> peConfA);
+
+    /**
+     * @brief Append new virtual channel configuration
+     *
+     * @param chConfA  Virtual channel configuration as a byte stream
+     *
+     * @return Index of added configuration
+     */
+    std::size_t appendChConfiguration(std::vector<uint8_t> chConfA);
+
+    bool writeResultImagetoFile(const std::string &pathA) const;
+
+    /**
+     * @brief Print kind of SystemC module
+     */
+    const char *kind() const override
+    {
+        return "VCGRA Testbench";
+    }
+
+    /**
+     * @brief Return name of SystemC module
+     */
+    void print(::std::ostream &os = ::std::cout) const override
+    {
+        os << name();
+    }
+
+    void dump(::std::ostream &os = ::std::cout) const override;
+
+    /**
+     * @brief Process the stimulation of the VCGRA instance
+     */
+    void stimuli();
+
+    /**
+     * @brief Initialize ports before starting simulation
+     */
+    void end_of_elaboration() override;
+
+  private:
+    std::string mImagePath;
+    //!< @brief Stores the path to the local image to process
+
+    const std::array<int16_t, 3 * 3> sobelx{1, 0, -1, 2, 0, -2, 1, 0, -1};
+    //!< Sobel coefficients in x-direction
+    const std::array<int16_t, 3 * 3> unit{0, 0, 0, 0, 1, 0, 0, 0, 0};
+    //!< Sobel coefficients in x-direction
+    const std::array<int16_t, 3 * 3> sobely{1, 2, 1, 0, 0, 0, -1, -2, -1};
+    //!< Sobel coefficients in y-direction
+
+    static constexpr uint8_t cInputSize{64};
+    ///!< Input image dimension
+    std::array<uint16_t, cInputSize * cInputSize> mInputValues;
+    ///!< Input image values
+    
+    static constexpr uint8_t cOutputSize{64};
+    ///!< Output image dimension
+    std::array<int16_t, cOutputSize * cOutputSize> mSobelxValues;
+    //!< Output image values after sobel operation in x direction
+    std::array<int16_t, cOutputSize * cOutputSize> mSobelyValues;
+    //!< Output image values after sobel operation in y direction
+    std::array<int16_t, cOutputSize * cOutputSize> mResultValues;
+    //!< Output image values
+
+    std::vector<std::vector<uint8_t>> mPeConfigs;
+    //!< @brief Stores the available configurations for the VCGRA PEs
+    std::vector<std::vector<uint8_t>> mChConfigs;
+    //!< @brief Stores the available configurations for the VCGRA virtual channels
+};
+
+} // namespace cgra
---
+++ src/vcgra_only.cpp
@@ -0,0 +1,114 @@
+/*
+ * vcgra_only.cpp
+ *
+ *  Created on: Dec 04, 2020
+ *      Author: andrewerner
+ */
+
+#include "testbench_vcgra_only.hpp"
+#include <systemc>
+#include <type_traits>
+
+namespace
+{
+
+const std::vector<std::vector<uint8_t>> cPeConfigs = {
+    {0x33, 0x33, 0x01, 0x01, 0x00, 0x10, 0x00, 0x80},
+    {0x38, 0x80, 0x01, 0x80, 0x00, 0x10, 0x00, 0x80},
+};
+
+const std::vector<std::vector<uint8_t>> cChConfigs = {
+    {0x05, 0x39, 0x77, 0x01, 0xAB, 0x05, 0x7F, 0x05, 0xAF, 0xB0},
+    {0x05, 0x26, 0xE4, 0x01, 0xAF, 0x05, 0x6F, 0x05, 0xAF, 0xB0},
+};
+
+template <typename T, typename G, typename U> void array_bind(T &p_inputs, G &p_outputs, U &p_signals)
+{
+
+    if (p_inputs.size() == p_outputs.size() && p_signals.size() == p_inputs.size()) {
+        for (std::size_t iter = 0; iter < p_inputs.size(); ++iter) {
+            p_inputs.at(iter).bind(p_signals.at(iter));
+            p_outputs.at(iter).bind(p_signals.at(iter));
+        }
+    }
+    else {
+        throw std::invalid_argument("Bounded arrays need to have the same sizes.");
+    }
+}
+
+} // namespace
+
+int sc_main(int argc, char **argv)
+{
+    cgra::Testbench testbench("vcgra_testbench", "../demo/lena.pgm");
+    cgra::VCGRA vcgra("VCGRA_Instance");
+
+    for (auto &peConf : cPeConfigs) {
+        testbench.appendPeConfiguration(peConf);
+    }
+
+    for (auto &chConf : cChConfigs) {
+        testbench.appendChConfiguration(chConf);
+    }
+
+    // Signals
+    sc_core::sc_clock s_clk("clk", 200, sc_core::SC_NS);
+    sc_core::sc_signal<cgra::start_type_t> s_start("start");
+    sc_core::sc_signal<cgra::reset_type_t> s_rst("rst");
+    sc_core::sc_signal<cgra::ready_type_t> s_ready("ready");
+    sc_core::sc_signal<cgra::pe_config_type_t> s_peConfig("peConfig");
+    sc_core::sc_signal<cgra::ch_config_type_t> s_chConfig("chConfig");
+    sc_core::sc_vector<sc_core::sc_signal<cgra::VCGRA::data_input_type_t>> s_inputs("inputs", vcgra.data_inputs.size());
+    sc_core::sc_vector<sc_core::sc_signal<cgra::VCGRA::data_output_type_t>> s_outputs("outputs",
+                                                                                      vcgra.data_outputs.size());
+
+    vcgra.clk.bind(s_clk);
+    testbench.clk.bind(s_clk);
+    vcgra.pe_config.bind(s_peConfig);
+    testbench.pe_config.bind(s_peConfig);
+    vcgra.ch_config.bind(s_chConfig);
+    testbench.ch_config.bind(s_chConfig);
+    vcgra.start.bind(s_start);
+    testbench.start.bind(s_start);
+    vcgra.rst.bind(s_rst);
+    testbench.rst.bind(s_rst);
+    testbench.ready.bind(s_ready);
+    vcgra.ready.bind(s_ready);
+
+    try {
+        array_bind(vcgra.data_inputs, testbench.data_inputs, s_inputs);
+        array_bind(testbench.data_outputs, vcgra.data_outputs, s_outputs);
+    }
+    catch (const std::invalid_argument &e) {
+        std::cerr << e.what() << '\n';
+        return EXIT_FAILURE;
+    }
+    catch (...) {
+        std::cerr << "Unknown exception\n";
+        return EXIT_FAILURE;
+    }
+
+    // create and setup trace file;
+    auto fp = sc_core::sc_create_vcd_trace_file("vcgra_only");
+
+    sc_core::sc_trace(fp, s_clk, s_clk.basename());
+    sc_core::sc_trace(fp, s_peConfig, s_peConfig.basename());
+    sc_core::sc_trace(fp, s_chConfig, s_chConfig.basename());
+    sc_core::sc_trace(fp, s_rst, s_rst.basename());
+    sc_core::sc_trace(fp, s_ready, s_ready.basename());
+    sc_core::sc_trace(fp, s_start, s_start.basename());
+    for(auto& in : s_inputs) {
+        sc_core::sc_trace(fp, in, in.basename());
+    }
+    for(auto& out : s_outputs) {
+        sc_core::sc_trace(fp, out, out.basename());
+    }
+
+    sc_core::sc_start();
+
+    testbench.writeResultImagetoFile("./vcgra_only_result_image.pgm");
+
+    sc_core::sc_close_vcd_trace_file(fp);
+
+    return EXIT_SUCCESS;
+}
